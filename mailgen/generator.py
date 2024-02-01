ddab1eb7a348b0e1986d9e26aa4df4602c063e0d
cvoluj <berezanskiydaniiil@gmail.com>
2024-02-01 05:29:13 +0200

    fixed generator.py

--- mailgen/generator.py
+++ mailgen/generator.py
@@ -19,19 +19,6 @@ kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
 user32 = ctypes.windll.user32
 user32.GetClipboardData.restype = ctypes.c_void_p
 
-def getClip6digit():
-    user32.OpenClipboard(0)
-    try:
-        if user32.IsClipboardFormatAvailable(CF_TEXT):
-            data = user32.GetClipboardData(CF_TEXT)
-            data_locked = kernel32.GlobalLock(data)
-            text = ctypes.c_char_p(data_locked)
-            value = text.value
-            kernel32.GlobalUnlock(data_locked)
-            return str(re.findall(r'(\d{6})', (str(value))))
-    finally:
-        user32.CloseClipboard()
-
 def getMail():
     user32.OpenClipboard(0)
     try:
@@ -41,21 +28,34 @@ def getMail():
             text = ctypes.c_char_p(data_locked)
             value = text.value
             kernel32.GlobalUnlock(data_locked)
-            if "@dropmail.me" in str(value)  or "@10mail.org"  in str(value)  or "@emlpro.com" in str(value) or "@emltmp.com" in str(value): # 
+            # "@dropmail.me" in str(value) or "@emltmp.com" in str(value) or "@spymail.one" in str(value) or
+            if  "@10mail.org" in str(value) or "@yomail.info" in str(value):
                 match = re.search(r'[\w.+-]+@[\w-]+\.[\w.-]+', str(value))
                 return str(match.group(0))
             return False
     finally:
         user32.CloseClipboard()
-webbrowser.open('https://google.com')
 
-time.sleep(10)
-pyautogui.keyDown('ctrlleft'); pyautogui.keyDown('shift'); pyautogui.typewrite('p'); pyautogui.keyUp('ctrlleft'); pyautogui.keyUp('shift')
-pyautogui.typewrite('https://account.proton.me/signup?plan=free\n')
-time.sleep(5)
+codeAvailable =  False
+def get_code(codeAvailable):
 
-
-
+    pyautogui.hotkey('ctrl', 'a')
+    print('worked')
+    time.sleep(5)
+    pyautogui.hotkey('ctrl', 'c')    
+    time.sleep(1)
+    user32.OpenClipboard(0)
+    try:
+        if user32.IsClipboardFormatAvailable(CF_TEXT):
+            data = user32.GetClipboardData(CF_TEXT)
+            data_locked = kernel32.GlobalLock(data)
+            text = ctypes.c_char_p(data_locked)
+            value = text.value
+            kernel32.GlobalUnlock(data_locked)
+            return re.search(r'\b\d{6}\b', str(value)).group(0)
+    finally:
+        user32.CloseClipboard()
+      
 def randomize(
                 _option_,
                 _length_
@@ -63,7 +63,7 @@ def randomize(
 
     if _length_ > 0 :
 
-        # Options:
+        # Options:6Ww$oRvfSVk95tyM  6Ww$oRvfSVk95tyM    
         #       -p      for letters, numbers and symbols
         #       -s      for letters and numbers
         #       -l      for letters only
@@ -97,35 +97,44 @@ def randomize(
     else:
         return 'error'
 
+
+webbrowser.open('https://account.proton.me/signup?plan=free')
+time.sleep(10)
+
 # Username
 _username_=randomize('-s',5)+randomize('-s',5)+randomize('-s',5)
 pyautogui.typewrite(_username_ + '\t\t')
+# pyautogui.typewrite('\t')
 print("Username:" + _username_)
 
 # Password
 _password_=randomize('-p',16)
 pyautogui.typewrite(_password_+'\t'+_password_+'\t')
 print("Password:" + _password_)
-
+time.sleep(2)
+pyautogui.typewrite(_password_)
 pyautogui.typewrite('\n')
+
 time.sleep(5)
-pyautogui.typewrite('\t\t\t\n')
+pyautogui.typewrite('\t\t\t')
+pyautogui.typewrite('\n')
+time.sleep(1)
 
 pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('t'); pyautogui.keyUp('ctrlleft')
 
 time.sleep(10)
 pyautogui.typewrite('https://dropmail.me/\n')
 
-
 pyautogui.keyDown('shift');pyautogui.keyDown('down'); pyautogui.keyUp('down'); pyautogui.keyUp('shift')
 time.sleep(10)
 
 newMail = True
+allow_pasting = None
 while True:
     if not newMail:
         pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('r'); pyautogui.keyUp('ctrlleft')
         time.sleep(5)
-    pyautogui.typewrite('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t')#28
+    pyautogui.typewrite('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t')
     pyautogui.keyDown('ctrlleft')
     pyautogui.keyDown('shiftleft')
     pyautogui.keyDown('shiftright')
@@ -133,60 +142,36 @@ while True:
     pyautogui.keyUp('shiftleft')
     pyautogui.keyUp('shiftright')
     pyautogui.keyUp('ctrlleft')
-    pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('c'); pyautogui.keyUp('ctrlleft')
+    pyautogui.hotkey('ctrl', 'c')
     newMail = getMail()
     if newMail:
         print("10 min mail: " + newMail)
         break
 
 pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
+
+pyautogui.hotkey('ctrl', 'v')
 time.sleep(1)
-#äpyautogui.typewrite(newMail)
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('v'); pyautogui.keyUp('ctrlleft')
-pyautogui.press('backspace')
-pyautogui.typewrite('\n')
-
-time.sleep(10)
-
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
-time.sleep(1)
-
-#pyautogui.typewrite('\t\t\t\t\t\t\t\t\t\t\t\t\t\n')
-
-#time.sleep(5)
-
-
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('a'); pyautogui.keyUp('ctrlleft')
-pyautogui.keyDown('ctrlleft'); pyautogui.typewrite('c'); pyautogui.keyUp('ctrlleft')
-
-
-pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
-time.sleep(5)
-pyautogui.typewrite(str(getClip6digit()) + '\n')
-
-
-time.sleep(5)
 pyautogui.typewrite('\n')
 time.sleep(5)
-pyautogui.typewrite('\t\t\t\t\n')
-time.sleep(1)
-pyautogui.typewrite('\t\n')
 
+pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
+time.sleep(18)
+code = get_code(codeAvailable=codeAvailable)
+pyautogui.keyDown('ctrlleft');  pyautogui.typewrite('\t'); pyautogui.keyUp('ctrlleft')
+pyautogui.typewrite(str(code))
+pyautogui.typewrite('\n')
+
+time.sleep(7)
+pyautogui.typewrite('\n')
+time.sleep(2)
+pyautogui.typewrite('\t\t\t')
+pyautogui.typewrite('\n')
+time.sleep(1)
+pyautogui.typewrite('\t')
+pyautogui.typewrite('\n')
 print(_username_+"@proton.me:" + _password_)
 
 logfile = open("accLog.txt", "a")
 logfile.write(_username_ + "@proton.me:" + _password_ + "\n")
 logfile.close()
-
-
-
-# CHAPTCHA
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-#pyautogui.typewrite('\t')
-
-#pyautogui.typewrite('\n')
